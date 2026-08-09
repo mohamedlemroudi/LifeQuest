706a2f4009076efea5d10eff46bb02fa16147e4d
Mohamed Lemroudi <mlemroudi700@gmail.com>
2026-08-09 21:53:36 +0200

    Create a player with different missions and display them in the terminal.

---
+++ app.py
@@ -0,0 +1,76 @@
+def show_player(player):
+    print("Jugador: " + player["name"])
+    print("Nivel: " + str(player["level"]))
+    print("XP: " + str(player["xp"]))
+    print("Niveles ganados: " + str(player["levels_gained"]))
+
+def level_up(player):
+    player["levels_gained"] = player["xp"] // 100
+    player["level"] += player["levels_gained"]
+    player["xp"] -= 100 * (player["levels_gained"])
+
+def gain_xp(player, amount):
+    player["xp"] += amount
+    return player["xp"]
+
+def show_mission(missions_list):
+    for i, mission in enumerate(missions_list, start=1):
+        print(f"{i}. {mission}")
+
+    print("0. Salir")
+
+def ask_mission(player, missions_list):
+    continuar = True
+    player["levels_gained"] = 0
+    
+    while continuar:
+        show_mission(missions_list)
+
+        try:
+            mission_number = int(input("Elige una misión: "))
+            
+            if 1 <= mission_number <= len(missions_list):
+                mission_selected = missions_list[mission_number - 1] 
+                print("Has completado:" + mission_selected)
+                print("Has ganado 50 XP.")
+
+                missions_list.remove(mission_selected)
+    
+                gain_xp(player, 50)
+    
+            elif mission_number == 0:
+                print("Saliendo del juego.")
+                continuar = False
+    
+            else:
+                print("No existe esa misión.")
+
+        except ValueError:
+            print("Respuesta no válida.")
+
+    if player["xp"] >= 100:
+        level_up(player)
+        
+        print("¡Felicidades! Has subido al nivel " + str(player["level"]) + ".")
+        print("¡Has ganado " + str(player["levels_gained"]) + " niveles!")
+
+
+player = {
+    "name": input("Enter your name: "),
+    "level": 1,
+    "xp": 0,
+    "levels_gained": 0
+}
+
+missions_list = [
+    "Estudiar Python",
+    "Hacer ejercicio",
+    "Leer 20 páginas",
+    "Meditar"
+]
+
+show_player(player)
+
+ask_mission(player, missions_list)
+
+show_player(player)
