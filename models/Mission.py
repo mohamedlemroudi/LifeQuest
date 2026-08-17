5dbc5fe4b8319086f2d63ecf31635b47e749d4ab
Mohamed Lemroudi <mlemroudi700@gmail.com>
2026-08-17 19:31:37 +0200

    Create the  and  functions in the  class.

--- models/Mission.py
+++ models/Mission.py
@@ -14,3 +14,20 @@ class Mission:
         }
 
         return self.xp_reward * multiplier[self.difficulty]
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "xp_reward": self.xp_reward,
+            "difficulty": self.difficulty
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            data["id"],
+            data["name"],
+            data["xp_reward"],
+            data["difficulty"]
+        )
